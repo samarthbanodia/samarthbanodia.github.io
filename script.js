6391c3063a4278fc923a49b2983f8b2c965d5900
samarthbanodia <sageofthesixpaths122@gmail.com>
2024-12-23 18:14:25 +0530

    timeline added

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 // Dark/Light Mode Toggle
-const themeToggle = document.getElementById('theme-toggle');
+// const themeToggle = document.getElementById('theme-switch');
 const body = document.body;
 
-themeToggle.addEventListener('click', () => {
+var themeToggle = document.getElementById('theme-switch__checkbox').checked;
+
+
+
+var checkbox = document.getElementById('theme-switch__checkbox');
+
+checkbox.addEventListener('change', function() {
+  body.classList.toggle('dark');
+
+  if (this.checked) {
+    console.log("Checkbox is checked..");
+  } else {
+    console.log("Checkbox is not checked..");
+  }
+});
+
+
+themeToggle.addEventListener( () => {
   body.classList.toggle('dark');
   const icon = themeToggle.querySelector('i');
-  if (body.classList.contains('dark')) {
+  if (body.classList.contains('dark') || themeToggle == 'true' ) {
+    console.log('dark modeeeee')
     icon.classList.remove('fa-moon');
     icon.classList.add('fa-sun');
   } else {
